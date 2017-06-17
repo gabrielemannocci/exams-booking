eca577403996c56e55cac016fa953e3abfc8fa0e
Gabriele Mannocci <gabrielemannocci@gmail.com>
2017-06-17 14:34:05 +0200

    Added project

---
+++ examsbooking/src/test/java/edu/unifi/tap/examsbooking/ExamsbookingApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.unifi.tap.examsbooking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamsbookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
