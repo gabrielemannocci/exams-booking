b0286ff8ae7c73e9a94bf4166a3ad8bca621388e
Gabriele Mannocci <gabrielemannocci@gmail.com>
2017-07-17 23:33:05 +0200

    removed StudentBuilder

--- examsbooking/src/test/java/edu/unifi/tap/exambooking/controller/HomeControllerTest.java
+++ examsbooking/src/test/java/edu/unifi/tap/exambooking/controller/HomeControllerTest.java
@@ -4,32 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,12 +27,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 //import webapp.example.demo.model.Exam;
 //import webapp.example.demo.services.interfaces.StudentService;
 
-import edu.unifi.tap.exambooking.controller.HomeController;
 import edu.unifi.tap.exambooking.exception.ExamsNotFoundException;
 import edu.unifi.tap.exambooking.exception.StudentAlreadyRegisteredForExamException;
 import edu.unifi.tap.exambooking.model.Exam;
 import edu.unifi.tap.exambooking.model.Student;
-import edu.unifi.tap.exambooking.model.StudentBuilder;
 import edu.unifi.tap.exambooking.services.interfaces.ExamService;
 import edu.unifi.tap.exambooking.services.interfaces.StudentService;
 
