f9aa79c3ba8fabbcf0c00683bb59e3845a2d4be3
Gabriele Mannocci <gabrielemannocci@gmail.com>
2017-09-11 23:45:59 +0200

    change ExamConverterImpl to remove vulnerability

--- examsbooking/src/main/java/edu/unifi/tap/exambooking/services/ExamConverterImpl.java
+++ examsbooking/src/main/java/edu/unifi/tap/exambooking/services/ExamConverterImpl.java
@@ -1,8 +1,12 @@
 package edu.unifi.tap.exambooking.services;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Service;
+
+import edu.unifi.tap.exambooking.controller.HomeController;
 import edu.unifi.tap.exambooking.exception.ExamsNotFoundException;
 import edu.unifi.tap.exambooking.model.Exam;
 import edu.unifi.tap.exambooking.services.interfaces.ExamService;
@@ -11,20 +15,22 @@ import edu.unifi.tap.exambooking.services.interfaces.ExamService;
 @Service("examConverter")
 public class ExamConverterImpl implements Converter<String, Exam> {
 
-@Autowired
-private ExamService examService;
+	final static Logger LOGGER = Logger.getLogger(HomeController.class);
 
-    @Override
-    public Exam convert(String arg0) {
-        Long id = new Long(arg0);
-        Exam exam = null;
-        try {
-        	exam = examService.findById(id);
+	@Autowired
+	private ExamService examService;
+
+	@Override
+	public Exam convert(String arg0) {
+		Long id = new Long(arg0);
+		Exam exam = null;
+		try {
+			exam = examService.findById(id);
 		} catch (ExamsNotFoundException e) {
-			e.printStackTrace();
+			LOGGER.error(e.getMessage());
 		}
-        return exam;
-    } 
+		return exam;
+	} 
 
 
 
