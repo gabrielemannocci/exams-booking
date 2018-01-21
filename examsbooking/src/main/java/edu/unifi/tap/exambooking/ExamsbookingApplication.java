e62e63b6e538519c4fc2b2177c0d6f05135a8d28
Gabriele Mannocci <gabrielemannocci@gmail.com>
2018-01-21 10:57:47 +0100

    webjars added

--- examsbooking/src/main/java/edu/unifi/tap/exambooking/ExamsbookingApplication.java
+++ examsbooking/src/main/java/edu/unifi/tap/exambooking/ExamsbookingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamsbookingApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ExamsbookingApplication.class, args);
 	}
